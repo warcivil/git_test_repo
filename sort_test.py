72296e173edceef1c632fe4a8925e08e84234219
warcivil <36387132+warcivil@users.noreply.github.com>
2021-07-03 20:15:38 +0500

    feat(code) - add sorting

---
+++ sort_test.py
@@ -0,0 +1,18 @@
+def bubble_sort(nums):  
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >=0 and key < arr[j] :
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key 
+
